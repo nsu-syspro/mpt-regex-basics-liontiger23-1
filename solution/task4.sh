61fdded44a2a3837848265a9f02dea155b74053a
Roman Baldin <roma.baldin@gmail.com>
2025-09-15 19:02:01 +0700

    task4 upd forgot space

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,3 +1,3 @@
 cp data/blok.txt data/blok_kids_edition.txt
-sed -i "s/Живи/Не\спи/g" data/blok_kids_edition.txt
+sed -i "s/Живи/Не\ спи/g" data/blok_kids_edition.txt
 sed -i "s/Умрёшь/Уснёшь/g" data/blok_kids_edition.txt
