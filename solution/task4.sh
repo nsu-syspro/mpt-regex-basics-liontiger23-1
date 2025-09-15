28580e4ee458ed67c5936e08b63ec20bdf018ab7
Roman Baldin <roma.baldin@gmail.com>
2025-09-15 19:00:44 +0700

    task4 upd block > blok

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,3 +1,3 @@
-cp data/blok.txt data/block_kids_edition.txt
-sed -i "s/Живи/Не\спи/g" data/block_kids_edition.txt
-sed -i "s/Умрёшь/Уснёшь/g" data/block_kids_edition.txt
+cp data/blok.txt data/blok_kids_edition.txt
+sed -i "s/Живи/Не\спи/g" data/blok_kids_edition.txt
+sed -i "s/Умрёшь/Уснёшь/g" data/blok_kids_edition.txt
